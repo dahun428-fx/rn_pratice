2758600bcd43caa49aebd668d0bbaa58f9281ccc
misumi-dahun-jung-01 <dh.a13y.jung@misumi.co.kr>
2024-11-15 18:03:30 +0900

    added

--- components/Empty.tsx
+++ components/Empty.tsx
@@ -1,11 +1,19 @@
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, Image} from 'react-native';
 import React from 'react';
 
 export default function Empty() {
-  const text = '야호! 할일이 없습니다';
-
+  const text = '야호! 할일이 없습니다.';
+  // const imageSrc = require('./assets/images/circle.png');
+  // const imageSrc = 'https://via.placeholder.com/150';
+  const imageSrc = require('./assets/images/young_and_happy.png');
   return (
     <View style={styles.block}>
+      <Image
+        // source={{uri: imageSrc}}
+        source={imageSrc}
+        style={styles.image}
+        resizeMode="contain"
+      />
       <Text style={styles.description}>{text}</Text>
     </View>
   );
@@ -20,5 +28,10 @@ const styles = StyleSheet.create({
   description: {
     fontSize: 24,
     color: '#9e9e9e',
+  },
+  image: {
+    width: 300,
+    height: 200,
+    // backgroundColor: 'gray',
   },
 });
